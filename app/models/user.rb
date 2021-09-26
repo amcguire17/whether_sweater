72984fb2e576f6cafd168e012c4c5f8fe58cb006
Amanda McGuire <aruwart001@gmail.com>
2021-09-25 19:54:56 -0600

    Update model password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates :password_confirmation, presence: true
   validates :password, presence: true
   validates :api_key, uniqueness: true
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe User do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
     it { should validate_presence_of(:password) }
-    it { should validate_presence_of(:password_confirmation) }
     it { should validate_uniqueness_of(:api_key) }
   end
 end
