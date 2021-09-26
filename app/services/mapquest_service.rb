e96d079108ed884cca77939b4c6642a06c890b81
Amanda McGuire <aruwart001@gmail.com>
2021-09-25 21:18:28 -0600

    Add directions mapquest method

--- app/services/mapquest_service.rb
+++ app/services/mapquest_service.rb
@@ -7,4 +7,15 @@ class MapquestService
     end
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.directions(start_location, end_location)
+    response = Faraday.get('https://www.mapquestapi.com/directions/v2/route') do |req|
+      req.params['key'] = ENV['MAPQUEST_KEY']
+      req.params['from'] = "#{start_location}"
+      req.params['to'] = "#{end_location}"
+
+      req.headers['Content-Type'] = 'application/json'
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -8,4 +8,17 @@ describe MapquestService, :vcr do
     expect(response[:results].first[:locations].first[:latLng]).to have_key(:lat)
     expect(response[:results].first[:locations].first[:latLng]).to have_key(:lng)
   end
+
+  it 'returns directions for locations' do
+    response = MapquestService.directions('anchorage,ak', 'seattle,wa')
+
+    expect(response).to be_a(Hash)
+    expect(response).to have_key(:route)
+    expect(response[:route]).to have_key(:formattedTime)
+    expect(response[:route][:locations].second).to have_key(:latLng)
+    expect(response[:route][:locations].first).to have_key(:adminArea5)
+    expect(response[:route][:locations].first).to have_key(:adminArea3)
+    expect(response[:route][:locations].second).to have_key(:adminArea5)
+    expect(response[:route][:locations].second).to have_key(:adminArea3)
+  end
 end
