d616d6db9bba49c9e01ecc4cb14f28dec2eb1e3b
Amanda McGuire <aruwart001@gmail.com>
2021-09-25 19:29:02 -0600

    Update model to create api key

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,11 @@ class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
   validates :password_confirmation, presence: true
   validates :password, presence: true
-  validates :api_key, uniqueness: true, presence: true
+  validates :api_key, uniqueness: true
 
   has_secure_password
 
-  before_create :set_api_key
-
-  def set_api_key
-    SecureRandom.hex(20)
+  before_create do
+    self.api_key =  SecureRandom.hex(20)
   end
 end
