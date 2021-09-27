f7224ae11679d169f4fc783719f17de622e913c2
Amanda McGuire <aruwart001@gmail.com>
2021-09-27 11:14:10 -0600

    Add testing for bad params

--- app/controllers/api/v1/book_search_controller.rb
+++ app/controllers/api/v1/book_search_controller.rb
@@ -1,8 +1,16 @@
 class Api::V1::BookSearchController < ApplicationController
   def index
-    coordinates = MapquestFacade.coordinates(params[:location])
-    weather = OpenWeatherService.weather_info(coordinates)
-    books = OpenLibraryFacade.books(params[:location], params[:quantity])
-    render json: BooksSerializer.format_data(books, weather, params[:location])
+    if params[:location].present? && params[:quantity].present?
+      if params[:quantity].to_i > 0
+        coordinates = MapquestFacade.coordinates(params[:location])
+        weather = OpenWeatherService.weather_info(coordinates)
+        books = OpenLibraryFacade.books(params[:location], params[:quantity])
+        render json: BooksSerializer.format_data(books, weather, params[:location])
+      else
+        render json: { error: 'Quantity should be greater than 0' }, status: :bad_request
+      end
+    else
+      render json: { error: 'Params should be given' }, status: :bad_request
+    end
   end
 end
--- spec/requests/api/v1/book_search_spec.rb
+++ spec/requests/api/v1/book_search_spec.rb
@@ -9,7 +9,6 @@ describe 'Book Search API', :vcr do
       expect(response).to be_successful
 
       books = JSON.parse(response.body, symbolize_names: true)[:data]
-
       expect(books[:id]).to eq('nil')
       expect(books[:type]).to eq('books')
       expect(books[:attributes]).to have_key(:destination)
@@ -22,6 +21,26 @@ describe 'Book Search API', :vcr do
         expect(book).to have_key(:title)
         expect(book).to have_key(:publisher)
       end
+    end
+
+    it 'returns an error if quantity is less than 0' do
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      get '/api/v1/book-search?location=austin,tx&quantity=-3', headers: headers
+
+      expect(response).to_not be_successful
+
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:error]).to eq('Quantity should be greater than 0')
+    end
+
+    it 'returns an error if params are not given' do
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+      get '/api/v1/book-search?location=&quantity=', headers: headers
+
+      expect(response).to_not be_successful
+
+      message = JSON.parse(response.body, symbolize_names: true)
+      expect(message[:error]).to eq('Params should be given')
     end
   end
 end
