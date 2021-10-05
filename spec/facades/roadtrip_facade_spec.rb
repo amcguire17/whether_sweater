4ea876eb92586bb1bf9d0b1ed47d6ea487c82cf4
Amanda McGuire <aruwart001@gmail.com>
2021-10-05 15:07:04 -0600

    Update for new fixture files

--- spec/facades/roadtrip_facade_spec.rb
+++ spec/facades/roadtrip_facade_spec.rb
@@ -8,17 +8,17 @@ describe RoadtripFacade, :vcr do
     expect(response.start_city).to eq('anchorage,ak')
     expect(response.end_city).to eq('whittier,ak')
     expect(response.travel_time).to eq('01 hours, 13 minutes')
-    expect(response.temperature).to eq(43.79)
-    expect(response.conditions).to eq("clear sky")
+    expect(response.temperature).to eq(42.44)
+    expect(response.conditions).to eq("light snow")
   end
 
   it 'returns weather info for destination' do
     response = RoadtripFacade.destination_weather('whittier,ak', 1)
 
     expect(response).to be_a(HourlyWeather)
-    expect(response.time).to eq("17:00:00")
-    expect(response.temperature).to eq(43.79)
-    expect(response.conditions).to eq("clear sky")
-    expect(response.icon).to eq("01d")
+    expect(response.time).to eq("15:00:00")
+    expect(response.temperature).to eq(42.44)
+    expect(response.conditions).to eq("light snow")
+    expect(response.icon).to eq("13d")
   end
 end
